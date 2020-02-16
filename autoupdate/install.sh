65d3a3d1515e1f5ebcc4f6e8886f0e6b1568b27d
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-16 18:49:34 +0100

    Autoupdate log to a known location

--- autoupdate/install.sh
+++ autoupdate/install.sh
@@ -2,5 +2,5 @@
 # setups the auto-update
 (
 	crontab -l | grep -v "dot-update"
-	echo "0 */2 * * * $HOME/.dotfiles/bin/dot-update > ${TMPDIR:-/tmp}/dot-update.log 2>&1"
+	echo "0 */2 * * * $HOME/.dotfiles/bin/dot-update > $HOME/.dotfiles/dot-update.log 2>&1"
 ) | crontab -
