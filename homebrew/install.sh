ab6b02f419f435ca015c6fb538c25a94b4c894ae
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-16 19:16:18 +0100

    Fix hombrew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,7 +2,12 @@
 
 [ "$(uname -s)" != "Darwin" ] && exit 0
 if ! brew >/dev/null; then
+    echo "› Installing Hombrew"
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew bundle dump --force --describe
+echo "› Installing Hombrew packages from Brewfile"
+brew bundle -v --file="$HOME/.dotfiles/hombrew/Brewfile"
+
+echo "› Update Brewfile"
+brew bundle dump --force --describe --file="$HOME/.dotfiles/hombrew/Brewfile"
