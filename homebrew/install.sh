c35f998a561fa032a4db68ce4ea25d7348176b43
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-16 19:32:16 +0100

    typo

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 [ "$(uname -s)" != "Darwin" ] && exit 0
-if ! brew list >/dev/null 2>&1; then
+if ! command -v brew >/dev/null 2>&1; then
     echo "› Installing Hombrew"
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo "› Installing Hombrew packages from Brewfile"
-brew bundle -v --file="$HOME/.dotfiles/hombrew/Brewfile"
+brew bundle -v --file="$HOME/.dotfiles/homebrew/Brewfile"
 
 echo "› Update Brewfile"
-brew bundle dump --force --describe --file="$HOME/.dotfiles/hombrew/Brewfile"
+brew bundle dump --force --describe --file="$HOME/.dotfiles/homebrew/Brewfile"
