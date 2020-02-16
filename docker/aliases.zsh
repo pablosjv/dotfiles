57ba04c4cd31e59fa93982b717bce577133bb501
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-16 17:57:02 +0100

    Modify docker clean alias

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,8 +1,12 @@
 #!/bin/sh
 function docker-prune() {
+    # Remove all docker data not used by a running container
     docker system prune --volumes -fa
 }
 
 function docker-clean() {
-    docker rmi $(docker images -q -f dangling=true)
+    # Clean docker trash generated more than five days ago
+    echo "Old Containers\t=> $(docker container prune -f --filter "until=120h")"
+    echo "Dangling images\t=> $(docker image prune -f --filter "until=120h")"
+    echo "Unused volumes\t=> $(docker volume prune -f)"
 }
