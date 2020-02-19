cd36af7bc8cacd9a06fd15f8425e71a5cf010e51
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-19 20:16:17 +0100

    Fix autoupdate

--- autoupdate/install.sh
+++ autoupdate/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # setups the auto-update
-# (
-# 	crontab -l | grep -v "dot-update"
-# 	echo "0 */2 * * * $HOME/.dotfiles/bin/dot-update > $HOME/.dotfiles/dot-update.log 2>&1"
-# ) | crontab -
+(
+    crontab -l | grep -v "dot-update"
+    echo "0 */2 * * * $HOME/.dotfiles/bin/dot-update > $HOME/.dotfiles/dot-update.log 2>&1"
+) | crontab -
