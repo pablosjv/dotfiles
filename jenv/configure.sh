9dd591326bd97f4bb1f6f94c5f419ec024311da9
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-19 20:06:23 +0100

    fix jenv configuartion

--- jenv/configure.sh
+++ jenv/configure.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # FIXME: Make it work in linux
 [ "$(uname -s)" != "Darwin" ] && exit 0
+
+# Hook Jenv to the shell of the script
+source $DOTFILES/jenv/path.zsh
+
 # Enable jenv plugins
 jenv enable-plugin export
 jenv enable-plugin springboot
@@ -13,3 +17,6 @@ for dir in /Library/Java/JavaVirtualMachines/*/; do
 	java_version=${dir%*/} # remove the trailing "/"
 	jenv add ${java_version}/Contents/Home
 done
+
+# Check everything is in order
+jenv doctor
