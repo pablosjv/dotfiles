4eb4d5d0a4d2849c6c08693b34bb206e0eb02caa
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-19 20:06:12 +0100

    fix typor

--- iterm/configure.sh
+++ iterm/configure.sh
@@ -4,4 +4,4 @@ sed "s;/Users/carlos;$HOME;g" \
 	"$DOTFILES"/iterm/com.googlecode.iterm2.plist.example >"$DOTFILES"/iterm/com.googlecode.iterm2.plist
 defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES/iterm"
 defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
-defaults write com.googlecode.iterm2 "ShowFullScreenTabBar" -nool true
+defaults write com.googlecode.iterm2 "ShowFullScreenTabBar" -bool true
