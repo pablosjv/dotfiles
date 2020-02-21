403add0415ed5d031e233683378abe32df94977d
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-21 21:44:53 +0100

    fix condition

--- antibody/configure.sh
+++ antibody/configure.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-if [ "$(uname -s)" != "Darwin" ]; then
-	echo "› Antibody should have been installed from hombrew"
+if [ "$(uname -s)" = "Darwin" ]; then
+    echo "› Antibody should have been installed from hombrew"
 else
-	# Set this condition in case we are testing this from a container
-	if command -v sudo >/dev/null; then
-		alias sh="sudo sh"
-	fi
-	curl -sL https://git.io/antibody | sh -s -- -b /usr/local/bin
+    # Set this condition in case we are testing this from a container
+    if command -v sudo >/dev/null; then
+        alias sh="sudo sh"
+    fi
+    curl -sL https://git.io/antibody | sh -s -- -b /usr/local/bin
 fi
 antibody bundle <"$DOTFILES/antibody/bundles.sh" >~/.zsh_plugins.sh
 antibody update
