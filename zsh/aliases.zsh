facf2da912a8c148195c08900ab1630dfcdad6eb
Pablo <pablo.sanjose@clarity.ai>
2020-02-21 18:20:23 +0100

    update the alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@
 alias reload='exec "$SHELL" -l'
 alias dot-date='git config --global dotfiles.lastupdate'
 alias dot-refresh='${DOTFILES}/scripts/update'
+alias dot-log='less -R ${DOTFILES}/dot-update.log'
