5ad183ec349bad5423c5c7387756c5c7dbd252a0
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-22 13:10:18 +0100

    fix jenv for linux

--- jenv/configure.sh
+++ jenv/configure.sh
@@ -11,12 +11,14 @@ jenv enable-plugin gradle
 jenv enable-plugin sbt
 jenv enable-plugin scala
 
-for java_version in /Library/Java/JavaVirtualMachines/*; do
-	if [ "$(uname -s)" == "Darwin" ]; then
-		jenv add "${java_version}/Contents/Home"
-	else
-		jenv add "${java_version}"
-	fi
+if [ "$(uname -s)" == "Darwin" ]; then
+    java_path="/Library/Java/JavaVirtualMachines/*/Contents/Home"
+else
+    java_path="/usr/lib/jvm/*"
+fi
+
+for java_version in $java_path; do
+    jenv add "${java_version}"
 done
 
 # Check everything is in order
