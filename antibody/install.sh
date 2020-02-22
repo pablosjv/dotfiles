ea49830006477716d6fb806bc23099d48d23727b
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-22 12:44:11 +0100

    test installation

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ "$(uname -s)" != "Darwin" ]; then
-	curl -sL https://git.io/antibody | sh -s -- -b /usr/local/bin
+    sudo curl -sL https://git.io/antibody | sh -s -- -b /usr/local/bin
 fi
