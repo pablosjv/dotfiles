465e8177ca614ad3dc57431508a40f88ff09d438
pablosjv <pablo.sanjose.villar@gmail.com>
2020-02-22 13:00:41 +0100

    try with sudo

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ "$(uname -s)" != "Darwin" ]; then
-    curl -sL https://git.io/antibody | sh -s -- -b /usr/local/bin
+    curl -sL https://git.io/antibody | sudo sh -s -- -b /usr/local/bin
 fi
