1ace1ba25bd92430d43ee4695332477a75422309
Pablo <pablo.sanjose@clarity.ai>
2020-03-05 15:56:12 +0100

    update docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -7,6 +7,7 @@ function docker-prune() {
 function docker-clean() {
     # Clean docker trash generated more than five days ago
     echo "Old Containers\t=> $(docker container prune -f --filter until=120h)"
+    echo "Old Networks\t=> $(docker network prune -f)"
     echo "Dangling images\t=> $(docker image prune -f --filter dangling=true --filter until=72h)"
     echo "Unused volumes\t=> $(docker volume prune -f)"
 }
