456430f28cf001ccf549f4b38aef197af89d177d
Pablo <pablo.sanjose@clarity.ai>
2020-04-07 15:45:11 +0200

    update docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -8,6 +8,13 @@ function docker-clean() {
     # Clean docker trash generated more than five days ago
     echo "Old Containers\t=> $(docker container prune -f --filter until=120h)"
     echo "Old Networks\t=> $(docker network prune -f)"
-    echo "Dangling images\t=> $(docker image prune -f --filter dangling=true --filter until=72h)"
+    echo "Dangling images\t=> $(docker image prune -f --filter dangling=true)"
     echo "Unused volumes\t=> $(docker volume prune -f)"
 }
+
+function dit() {
+    local cmd="${2:-bash}"
+    docker run -it --rm $1 $cmd
+}
+
+alias docker-it="docker run -it --rm"
