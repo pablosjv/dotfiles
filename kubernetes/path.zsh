e019e90d9890e8c6a641c5f11f21dd72096b12a5
Pablo <pablo.sanjose.villar@gmail.com>
2020-06-18 18:20:10 +0200

    dynamic kubeconfig load

--- kubernetes/path.zsh
+++ kubernetes/path.zsh
@@ -2,4 +2,9 @@
 export PATH="$HOME/.krew/bin:$PATH"
 
 export KUBEHOME=~/.kube
-export KUBECONFIG=${KUBEHOME}/config/dev.yml:${KUBEHOME}/config/pre.yml:${KUBEHOME}/config/prod.yml:${KUBEHOME}/config/mgmt.yml
+
+KUBECONFIG=
+
+for filename in ${KUBEHOME}/config/*.yml; do
+    KUBECONFIG=${KUBECONFIG}:${filename}
+done
