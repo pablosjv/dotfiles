44ae200368a83489e49b6b095ef05e6e9347cabe
pablosjv <pablo.sanjose.villar@gmail.com>
2020-06-18 18:59:38 +0200

    fix if no configuration is set

--- kubernetes/path.zsh
+++ kubernetes/path.zsh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/zsh
 export PATH="$HOME/.krew/bin:$PATH"
 
 export KUBEHOME=~/.kube
 
 KUBECONFIG=
 
+setopt +o nomatch
 for filename in ${KUBEHOME}/config/*.yml; do
     KUBECONFIG=${KUBECONFIG}:${filename}
 done
