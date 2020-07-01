ef33f31d538cb29df94fb727cf09533fb105351c
Pablo <pablo.sanjose.villar@gmail.com>
2020-07-01 10:54:48 +0200

    add aws aliases for most used services

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -8,3 +8,7 @@ alias aws-ec2-running="aws ec2 describe-instances --query 'Reservations[*].Insta
 # }
 
 alias aws-ec2-running-count="aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --output text | wc -l"
+
+alias s3="aws s3"
+alias ec2="aws ec2"
+alias emr="aws emr"
