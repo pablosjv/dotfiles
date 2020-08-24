d0d49ca7ddc0717e33e786fd6124af47d90f73d0
Pablo <pablo.sanjose.villar@gmail.com>
2020-08-24 15:40:40 +0200

    put kubeconfig in function

--- kubernetes/path.zsh
+++ kubernetes/path.zsh
@@ -3,13 +3,16 @@ export PATH="$HOME/.krew/bin:$PATH"
 
 export KUBEHOME=~/.kube
 
-unset KUBECONFIG
+function kubeconfig {
+    unset KUBECONFIG
+    setopt +o nomatch
+    for filename in ${KUBEHOME}/configs/*.yml; do
+        if [[ -z "${KUBECONFIG}" ]]; then
+            export KUBECONFIG=${filename}
+        else
+            export KUBECONFIG=${filename}:${KUBECONFIG}
+        fi
+    done
+}
 
-setopt +o nomatch
-for filename in ${KUBEHOME}/configs/*.yml; do
-    if [[ -z "${KUBECONFIG}" ]]; then
-        export KUBECONFIG=${filename}
-    else
-        export KUBECONFIG=${filename}:${KUBECONFIG}
-    fi
-done
+kubeconfig
