f3f6e1fe19492e602362fb486afae24e6cc16503
Pablo <pablo.sanjose.villar@gmail.com>
2020-10-01 15:18:55 +0200

    add myip alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,5 @@ alias dot-refresh='${DOTFILES}/scripts/update'
 alias dot-log='less -R ${DOTFILES}/dot-update.log'
 alias e-dot='e ${DOTFILES}'
 alias zsh-profile='(time zsh -i -c exit) 2>&1 >/dev/null | (head -10 ; tail -1)'
+
+alias myip='curl https://checkip.amazonaws.com/'
