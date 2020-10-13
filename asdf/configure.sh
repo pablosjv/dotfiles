cc1c5e5f2cf93a34aca2c9b54dcef2bb5c660106
pablosjv <pablo.sanjose.villar@gmail.com>
2020-10-13 09:39:20 +0200

    configure for python

--- asdf/configure.sh
+++ asdf/configure.sh
@@ -9,3 +9,7 @@ asdf plugin add java
 
 # Add java versions
 asdf install java openjdk-11
+
+# Add python versions
+asdf install python 3.7.9
+asdf install python 3.9.0
--- asdf/path.zsh
+++ asdf/path.zsh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-$(brew --prefix asdf)/asdf.sh
+source $(brew --prefix asdf)/asdf.sh
 
 # Automatically sets JAVA_HOME
 . ~/.asdf/plugins/java/set-java-home.zsh
