dd99fdaed444ee4b0ec79d63fa934d3784ef0090
Pablo <pablo.sanjose.villar@gmail.com>
2020-11-05 10:05:20 +0100

    feat: add more asdf version

--- asdf/configure.sh
+++ asdf/configure.sh
@@ -9,7 +9,10 @@ asdf plugin add java
 
 # Add java versions
 asdf install java openjdk-11
+asdf install java adoptopenjdk-8.0.272+10
+asdf install java adoptopenjdk-11.0.9+11
 
 # Add python versions
 asdf install python 3.7.9
+asdf install python 3.8.0
 asdf install python 3.9.0
