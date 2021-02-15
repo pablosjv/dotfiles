a49005068e20f291bc8c77802755af720453c488
Pablo <pablo.sanjose.villar@gmail.com>
2021-02-15 13:17:52 +0100

    feat: homebrew wrapper updated with useful functions

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,4 +1,32 @@
 #!/bin/sh
+
+brew-bump() {
+    tput bold
+    echo "--> Bumping homebrew..."
+    tput sgr0
+    command brew update
+    brew outdated | xargs brew fetch
+    command brew upgrade
+    brew cleanup
+}
+
+brew-cleanup() {
+    tput bold
+    echo "--> Cleanup homebrew..."
+    tput sgr0
+    (cd "$(brew --repo)" && git prune && git gc)
+    command brew cleanup
+    rm -rf "$(brew --cache)"
+}
+
+brew-repair() {
+    tput bold
+    echo "--> Try to repair homebrew..."
+    tput sgr0
+    (cd "$(brew --repo)" && git fetch && git reset --hard origin/master)
+    brew update
+}
+
 if command -v brew >/dev/null 2>&1; then
     brew() {
         case "$1" in
@@ -7,6 +35,9 @@ if command -v brew >/dev/null 2>&1; then
             ;;
         bump)
             brew-bump
+            ;;
+        repair)
+            brew-repair
             ;;
         *)
             command brew "$@"
