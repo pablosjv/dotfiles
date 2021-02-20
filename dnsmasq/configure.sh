b7c4d9047308298920411760ae53d18b69b2fbba
pablosjv <pablo.sanjose.villar@gmail.com>
2021-02-20 12:20:05 +0100

    feat: don't restart dnsmasq if the resolver is found

--- dnsmasq/configure.sh
+++ dnsmasq/configure.sh
@@ -5,12 +5,10 @@
 mkdir -pv "$(brew --prefix)/etc"
 ln -sf "${DOTFILES}/dnsmasq/dnsmasq.conf" "$(brew --prefix)/etc/dnsmasq.conf"
 
-if command -v sudo >/dev/null; then
-    sudo brew services restart dnsmasq
-else
-    brew services restart dnsmasq
-fi
 [ -L "/etc/resolver" ] && exit 0
 if command -v sudo >/dev/null; then
     sudo ln -sf "${DOTFILES}/dnsmasq/resolver/" "/etc/resolver"
+    sudo brew services restart dnsmasq
+else
+    brew services restart dnsmasq
 fi
