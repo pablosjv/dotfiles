c442d802d6eacd74d055d8df3e2562e5bf2bfe38
pablosjv <pablo.sanjose.villar@gmail.com>
2021-02-20 12:19:30 +0100

    feat: configura autoswitch virtualenv

--- python/path.zsh
+++ python/path.zsh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 # export PATH="$PATH:$HOME/Library/Python/3.8/bin"
+
+### Autho switch configuration
+# NOTE: install default packages for development
+export AUTOSWITCH_DEFAULT_REQUIREMENTS="$HOME/.requirements.txt"
+# NOTE: Use asdf python version for that directory
+export AUTOSWITCH_DEFAULT_PYTHON="/Users/pablosjv/.asdf/shims/python"
