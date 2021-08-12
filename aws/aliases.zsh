9eb6313b5d22a4c723f3aa2235beb180e30e77d4
Pablo <pablo.sanjose.villar@gmail.com>
2021-08-12 13:37:28 +0200

    feat: new aws aliases

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 alias aws-ec2-running="aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,InstanceType]' --filters Name=instance-state-name,Values=running --output table"
+alias aws-ec2-running-count="aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --output text | wc -l"
 
 # TODO: List based on arguments
-# aws-ec2-list () {
+# aws-ec2-list () {
 #     aws ec2 describe-instances --query 'Reservations[*].Instances[*].[$@]' --filters Name=instance-state-name,Values=running --output table
 # }
-
-alias aws-ec2-running-count="aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --output text | wc -l"
 
 alias s3="aws s3"
 alias ec2="aws ec2"
 alias emr="aws emr"
+
+aws-pip-login (){
+    aws codeartifact login --tool pip --domain ${1} --repository ${2:-python}
+}
