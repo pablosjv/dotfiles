eb63807276d7be77e6f4ce71acd82d765c75c136
Pablo <pablo.sanjose.villar@gmail.com>
2022-08-07 13:16:10 +0200

    feat: alias for sagemaker train logs

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -11,6 +11,7 @@ alias aws-ec2-running-count="aws ec2 describe-instances --query 'Reservations[*]
 alias s3="aws s3"
 alias ec2="aws ec2"
 alias emr="aws emr"
+alias sm-train-logs="aws logs tail /aws/sagemaker/TrainingJobs --since 5m --follow --log-stream-name-prefix"
 
 aws-pip-login (){
     aws codeartifact login --tool pip --domain ${1} --repository ${2:-python}
