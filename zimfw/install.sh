5f5eccad5c1da257f11b36f9c363ec7baf35ebcf
pablosjv <pablo.sanjose.villar@gmail.com>
2022-09-06 18:27:14 +0200

    fix zim installation

--- zimfw/install.sh
+++ zimfw/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-if ! command -v zimfw >/dev/null 2>&1; then
+if ! command -v ls "${HOME}/.zim" >/dev/null 2>&1; then
     curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 fi
--- zimfw/path.zsh
+++ zimfw/path.zsh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 # export ZIM_HOME=${HOME}/.dotfiles/zimfw
+# ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim
