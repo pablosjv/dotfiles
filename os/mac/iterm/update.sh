9b3aaff3c9b411fe49c3be1f66b1cc54f51627a7
Pablo <pablo.sanjose.villar@gmail.com>
2022-09-28 09:32:52 +0200

    fix: iterm update script

--- os/mac/iterm/update.sh
+++ os/mac/iterm/update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 [ "$(uname -s)" != "Darwin" ] && exit 0
 sed "s;$HOME;/Users/Pablo;g" \
-    "$DOTFILES"/iterm/com.googlecode.iterm2.plist >"$DOTFILES"/iterm/com.googlecode.iterm2.plist.example
+    "$DOTFILES"/os/mac/iterm/com.googlecode.iterm2.plist >"$DOTFILES"/os/mac/iterm/com.googlecode.iterm2.plist.example
