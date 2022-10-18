ab445e7f4e77c580b5ef3d1743d415dcf648fdb6
Pablo <pablo.sanjose.villar@gmail.com>
2022-10-18 16:40:09 +0200

    add s3-duh

--- tools/aws/functions.sh
+++ tools/aws/functions.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 # TODO: List based on arguments
-aws-ec2-list () {
+aws-ec2-list() {
     aws ec2 describe-instances --query 'Reservations[*].Instances[*].[$@]' --filters Name=instance-state-name,Values=running --output table
 }
 
-aws-pip-login (){
+aws-pip-login() {
     aws codeartifact login --tool pip --domain "${1}" --repository "${2:-python}"
+}
+
+s3-duh() {
+    aws s3 ls --recursive --summarize --human-readable "${1}" | tail -n 2
 }
