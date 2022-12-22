94759841ba929edf97d0e5fd89a5a38e9bc2d1f4
Pablo <pablo.sanjose.villar@gmail.com>
2022-12-22 16:41:09 +0100

    fix asdf exports

--- tools/asdf/exports.sh
+++ tools/asdf/exports.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-# shellcheck source=/usr/local/opt/asdf/asdf.sh
-. "$(brew --prefix asdf)/asdf.sh"
+# shellcheck source=/usr/local/opt/asdf/libexec/asdf.sh
+. "$(brew --prefix asdf)/libexec/asdf.sh"
 
 # Automatically sets JAVA_HOME
 . ~/.asdf/plugins/java/set-java-home.zsh
