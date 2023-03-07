1164e0171aee98b39a747fce7ce49d06872474dd
pablo.sanjose <pablo.sanjose.villar@gmail.com>
2023-03-07 11:59:06 +0100

    fix homebrew path

--- tools/autojump/exports.sh
+++ tools/autojump/exports.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
+[ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
