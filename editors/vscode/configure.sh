c0589462470641f641cd1b8cc33033e3ca1c36b8
pablo.sanjose <pablo.sanjose.villar@gmail.com>
2024-02-28 10:25:15 +0100

    remove custom vscode extensions instalation

--- editors/vscode/configure.sh
+++ editors/vscode/configure.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env sh
 
-install_vscode_extensions() {
-    EXTENSIONS_FILE="$DOTFILES/editors/vscode/extensions.txt"
-    echo "  › Installing extensions from $EXTENSIONS_FILE"
-    tput setaf 2
-    printf "    [ "
-    while read -r module; do
-        # TODO: hide output and use progress bar
-        code --install-extension "$module" >/dev/null 2>&1
-        printf "#"
-    done <"$EXTENSIONS_FILE"
-    printf " ] \n"
-    tput sgr0
-    echo "  › Extensions installed"
-}
+set -e
 
 if command -v code >/dev/null; then
     if [ "$(uname -s)" = "Darwin" ]; then
@@ -22,6 +9,4 @@ if command -v code >/dev/null; then
         VSCODE_HOME="$HOME/.config/Code"
     fi
     mkdir -p "$VSCODE_HOME/User"
-    # NOTE: extensions are installed with homebrew now
-    # install_vscode_extensions
 fi
--- editors/vscode/update.sh
+++ editors/vscode/update.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-code --list-extensions >"$DOTFILES/editors/vscode/extensions.txt"
 jq --indent 4 -S <"$DOTFILES/editors/vscode/settings.json" >"$DOTFILES/editors/vscode/settings.json.new"
 mv "$DOTFILES/editors/vscode/settings.json.new" "$DOTFILES/editors/vscode/settings.json"
