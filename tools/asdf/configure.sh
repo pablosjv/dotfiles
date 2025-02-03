bc0b19c81b865d9d11a92f3b533623241dc6d566
pablo.sanjose <pablo.sanjose.villar@gmail.com>
2025-02-03 12:04:33 +0100

    update tools and add uv

--- tools/asdf/configure.sh
+++ tools/asdf/configure.sh
@@ -6,15 +6,13 @@ chmod +x "$(brew --prefix asdf)/asdf.sh"
 # Add used plugins
 asdf plugin add python
 asdf plugin add poetry
+asdf plugin add uv
 asdf plugin-add golang
 asdf plugin add java
-asdf plugin-add scala
-asdf plugin-add sbt
-asdf plugin-add maven
-asdf plugin-add gradle
 asdf plugin-add nodejs
 asdf plugin-add yarn
 asdf plugin-add awscli
+# Sops plugin for the asdf version manager
 asdf plugin-add sops
 # Pluging for faster env resolution https://github.com/asdf-community/asdf-direnv
 asdf plugin-add direnv
@@ -23,10 +21,3 @@ asdf plugin add dasel
 
 # Install all packages in ./etc/tools-versions
 asdf install
-
-# Add legacy scala versions
-asdf install scala 2.13.10
-
-# Add legacy python versions
-asdf install python 3.7.9
-asdf install python 3.8.10
