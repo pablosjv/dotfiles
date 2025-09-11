dd41036930a85acbb67afab2d8bc37fbc5422e7c
pablosjv <pablo.sanjose.villar@gmail.com>
2025-09-11 12:32:05 +0200

    chore: add node and ruby to asdf and remove legacy stuff

--- tools/asdf/configure.sh
+++ tools/asdf/configure.sh
@@ -4,15 +4,13 @@
 asdf plugin add python
 asdf plugin add poetry
 asdf plugin add uv
-asdf plugin-add golang
+asdf plugin add golang
 asdf plugin add java
-asdf plugin-add nodejs
-asdf plugin-add yarn
-asdf plugin-add awscli
-# Sops plugin for the asdf version manager
-asdf plugin-add sops
+asdf plugin add nodejs
+asdf plugin add yarn
+asdf plugin add ruby
 # Pluging for faster env resolution https://github.com/asdf-community/asdf-direnv
-asdf plugin-add direnv
+asdf plugin add direnv
 # Dasel tool https://github.com/TomWright/dasel
 asdf plugin add dasel
 
