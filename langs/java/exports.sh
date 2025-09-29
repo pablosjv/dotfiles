1b909b50a28e055835b164568191d3a4cfe7ad0e
pablosjv <pablo.sanjose.villar@gmail.com>
2025-09-29 16:48:12 +0200

    feat: add Android SDK configuration and asdf java home integration

--- langs/java/exports.sh
+++ langs/java/exports.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
 
 export MAVEN_OPTS="-Xmx1024m"
+export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
+export ANDROID_HOME=$HOME/Library/Android/sdk
+
+# PATH updates
+export PATH="$PATH:$ANDROID_HOME/emulator"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
+export PATH="$PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin"
+
+. $HOME/.asdf/plugins/java/set-java-home.zsh
