07243ad5a0f01dd5758c62af1c9d26eb9f5ba597
pablosjv <pablo.sanjose.villar@gmail.com>
2025-11-24 11:45:52 +0100

    feat(golang): export golang env with aws if available

--- langs/golang/exports.sh
+++ langs/golang/exports.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
-. ${ASDF_DATA_DIR:-$HOME/.asdf}/plugins/golang/set-env.zsh
-# export GOROOT=/usr/local/opt/go/libexec
-# export GOPATH="$PROJECTS/go"
+if command -v asdf >/dev/null 2>&1; then
+    . "${ASDF_DATA_DIR:-$HOME/.asdf}/plugins/golang/set-env.zsh"
+else
+    export GOROOT=/usr/local/opt/go/libexec
+    export GOPATH="${PROJECTS:-$HOME/projects}/go"
+fi
 
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
