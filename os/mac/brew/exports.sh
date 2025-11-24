54c22814b53d8b4f9865a14c5dc1cd483b4903f1
pablosjv <pablo.sanjose.villar@gmail.com>
2025-11-24 11:32:22 +0100

    feat(brew): disable autoupdate for homebrew

--- os/mac/brew/exports.sh
+++ os/mac/brew/exports.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
-export HOMEBREW_PREFIX="/opt/homebrew";
-export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-export HOMEBREW_REPOSITORY="/opt/homebrew";
+export HOMEBREW_PREFIX="/opt/homebrew"
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar"
+export HOMEBREW_REPOSITORY="/opt/homebrew"
 
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
-export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}"
+export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:"
+export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}"
 
 # Hide hints on homebrew behavior
 export HOMEBREW_NO_ENV_HINTS=1
+
+# Disable Homebrew's auto-update
+export HOMEBREW_NO_AUTO_UPDATE=1
