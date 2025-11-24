fba0bc335d4cc8acf7dd10e89bdf38aaad817f5d
pablosjv <pablo.sanjose.villar@gmail.com>
2025-11-24 11:31:30 +0100

    feat: use asdf for golang exports

--- langs/golang/exports.sh
+++ langs/golang/exports.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+
+. ${ASDF_DATA_DIR:-$HOME/.asdf}/plugins/golang/set-env.zsh
 # export GOROOT=/usr/local/opt/go/libexec
 # export GOPATH="$PROJECTS/go"
+
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
