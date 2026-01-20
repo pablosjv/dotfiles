437c10cc60a5d8fcc2f87b5640abc36a719ac458
pablosjv <pablo.sanjose.villar@gmail.com>
2026-01-20 16:45:00 +0100

    chore: remove iterm from the dotfiles

--- os/mac/iterm/configure.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-[ "$(uname -s)" != "Darwin" ] && exit 0
-
-# sed "s;/Users/Pablo;$HOME;g" \
-#     "$DOTFILES"/iterm/com.googlecode.iterm2.plist.example >"$DOTFILES"/iterm/com.googlecode.iterm2.plist
-
-defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES/os/mac/iterm"
-defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
-defaults write com.googlecode.iterm2 "ShowFullScreenTabBar" -bool true
--- os/mac/iterm/update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-[ "$(uname -s)" != "Darwin" ] && exit 0
-# sed "s;$HOME;/Users/Pablo;g" \
-#     "$DOTFILES"/os/mac/iterm/com.googlecode.iterm2.plist >"$DOTFILES"/os/mac/iterm/com.googlecode.iterm2.plist.example
