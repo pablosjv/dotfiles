4216ad7ec9bce8e445e0e98884a2c68d64a2557b
pablosjv <pablo.sanjose.villar@gmail.com>
2026-03-02 10:51:27 +0100

    chore(xcode): upgrade xcode version

--- os/mac/xcode/install.sh
+++ os/mac/xcode/install.sh
@@ -11,29 +11,30 @@ xcode-select --install || user "Error running the install: probably because it i
 
 sudo xcodebuild -license accept || true
 
-# === Install Xcode 16.2 with credentials ===
-info "> Checking Xcode 16.2..."
+# === Install Xcode version with credentials ===
+XCODE_VERSION="26.3"
+info "> Checking Xcode version $XCODE_VERSION..."
 
-if ! xcodes installed | grep -q "16.2"; then
-  if [[ -z "$XCODES_USERNAME" || -z "$XCODES_PASSWORD" ]]; then
-    fail "⚠️ Environment variables XCODES_USERNAME and/or XCODES_PASSWORD are not set. Skipping Xcode installation."
-  else
-    info "Installing Xcode 16.2..."
-    xcodes install 16.2
-  fi
+if ! xcodes installed | grep -q "$XCODE_VERSION"; then
+    if [[ -z "$XCODES_USERNAME" || -z "$XCODES_PASSWORD" ]]; then
+        fail "⚠️ Environment variables XCODES_USERNAME and/or XCODES_PASSWORD are not set. Skipping Xcode installation."
+    else
+        info "Installing Xcode $XCODE_VERSION..."
+        xcodes install $XCODE_VERSION
+    fi
 else
-  success "Xcode 16.2 already installed ✓"
+    success "Xcode $XCODE_VERSION already installed ✓"
 fi
 
-info "Selecting Xcode 16.2..."
-xcodes select 16.2
-success "Xcode 16.2 already selected ✓"
+info "Selecting Xcode $XCODE_VERSION..."
+xcodes select $XCODE_VERSION
+success "Xcode $XCODE_VERSION already selected ✓"
 
 # === Install Simulator ===
 info "> Checking iOS Simulator 18.2..."
 if ! xcodes runtimes | grep -q "iOS 18.2"; then
-  info "Installing iOS Simulator 18.2..."
-  xcodes runtimes install "iOS 18.2" || fail "⚠️ Could not install iOS 18.2 simulator. You may need to do this manually."
+    info "Installing iOS Simulator 18.2..."
+    xcodes runtimes install "iOS 18.2" || fail "⚠️ Could not install iOS 18.2 simulator. You may need to do this manually."
 else
-  success "iOS Simulator 18.2 already installed ✓"
+    success "iOS Simulator 18.2 already installed ✓"
 fi
