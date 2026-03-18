e1eaddf40315865420448deded8f7be2220a038f
pablosjv <pablo.sanjose.villar@gmail.com>
2026-03-18 10:36:54 +0100

    chore: clean up unused files

--- editors/vscode/update.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-jq --indent 4 -S <"$DOTFILES/editors/vscode/settings.json" >"$DOTFILES/editors/vscode/settings.json.new"
-mv "$DOTFILES/editors/vscode/settings.json.new" "$DOTFILES/editors/vscode/settings.json"
